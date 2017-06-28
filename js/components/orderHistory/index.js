cf5920836739cc026b4f1b98e482d9c6cf3e0e97
sunnyworld2009 <sunnygiri963@gmail.com>
2017-06-28 21:32:39 +0530

    Add down arrow to order history page

--- js/components/orderHistory/index.js
+++ js/components/orderHistory/index.js
@@ -32,41 +32,42 @@ class OrderHistory extends Component {
   static navigationOptions = {
     header: null
   };
-
+  
   _renderHeader(section) {
     return (
       <View style={styles.accordionHeader}>
-               <View style={{alignItems: 'center', flex: 2 }}>
-                <Text style={{ fontSize: 20 }} note>Booking Details</Text>
-               </View>
+        <View style={{alignItems: 'center', flex: 2 }}>
+          <Text style={{ fontSize: 20 }} note>Booking Details</Text>
+          <Icon name="ios-arrow-round-down" color='grey' />
+        </View>
       </View>
     );
   }
-
+  
   _renderContent(section) {
     return (
       <View style={styles.accordionBody}>
-      <Content>
-                <ListItem >
-                  <Text>Client Name - XYZ</Text>
-                </ListItem>
-                <ListItem>
-                  <Text>Client Location - XYZ</Text>
-                </ListItem>
-                <ListItem>
-                  <Text>Estimated Time - XYZ</Text>
-                </ListItem>
-                <ListItem>
-                  <Text>Pickup & Drop Coordinates - XYZ </Text>
-                </ListItem>
-                <ListItem>
-                  <Text>Payment Mode - XYZ</Text>
-                </ListItem>
-              </Content>
+        <Content>
+          <ListItem >
+            <Text>Client Name - XYZ</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Client Location - XYZ</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Estimated Time - XYZ</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Pickup & Drop Coordinates - XYZ </Text>
+          </ListItem>
+          <ListItem>
+            <Text>Payment Mode - XYZ</Text>
+          </ListItem>
+        </Content>
       </View>
     );
   }
-
+  
   render() {
     const { props: { name, index, list } } = this;
     return (
@@ -77,27 +78,27 @@ class OrderHistory extends Component {
               <Icon name="ios-arrow-back" />
             </Button>
           </Left>
-
+          
           <Body>
             <Title>Order History</Title>
           </Body>
-
+          
           <Right>
             <Button
               transparent
               onPress={() => this.props.navigation.navigate("DrawerOpen")}
-            >
+              >
               <Icon name="ios-menu" />
             </Button>
           </Right>
         </Header>
-
+        
         <Content padder>
           <Accordion
             sections={SECTIONS}
             renderHeader={this._renderHeader}
             renderContent={this._renderContent}
-          />
+            />
         </Content>
       </Container>
     );
