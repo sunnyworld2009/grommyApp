3b5d99fe2eea749a267fe39005a3a70118a93396
sunnyworld2009 <sunnygiri963@gmail.com>
2017-06-30 16:09:15 +0530

    Show driver profile on profile page

--- js/components/profile/index.js
+++ js/components/profile/index.js
@@ -19,12 +19,19 @@ import {
   Thumbnail,
 } from "native-base";
 
+const mapStateToProps = state => ({
+  userData: state.user.data,
+  list: state.list.list,
+  bookingData: state.home.bookingData
+});
+
 class Profile extends Component {
   static navigationOptions = {
     header: null
   };
   render() {
     const { props: { name, index, list } } = this;
+    const userData =  this.props.userData;
     return (
       <Container>
         <Header>
@@ -33,58 +40,59 @@ class Profile extends Component {
               <Icon name="ios-arrow-back" />
             </Button>
           </Left>
-
+          
           <Body>
             <Title>Profile</Title>
           </Body>
-
+          
           <Right>
             <Button
               transparent
               onPress={() => this.props.navigation.navigate("DrawerOpen")}
-            >
+              >
               <Icon name="ios-menu" />
             </Button>
           </Right>
         </Header>
-
+        
         <Content padder>
-        <Card style={{flex: 0}}>
-          <CardItem>
-            <Body>
-            <View style={{flex: 1, flexDirection:'row', alignItems: 'center'}}>
-              <Image style={{alignSelf: 'center'}} source={{uri: 'https://static1.squarespace.com/static/50de3e1fe4b0a05702aa9cda/t/50eb2245e4b0404f3771bbcb/1357589992287/ss_profile.jpg'}} style={{height: 200, width: 200, flex: 1}}/>
-            </View>
-            <List>
-              <ListItem >
-                <Text>Name - William Shaw</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Contact- 97764646778</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Address - 221B Baker Street</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Vehicle Number - MH04 MQ6535</Text>
-              </ListItem>
-              <ListItem>
-                <Text>License Number - 75674476689866</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Insurance No. - 8787076566588</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Insurance Expiry Date - 22/04/2020</Text>
-              </ListItem>
-            </List>
-            </Body>
-          </CardItem>
-        </Card>
+          <Card style={{flex: 0}}>
+            <CardItem>
+              <Body>
+                {
+                  !!userData.driver_photo &&
+                  <View style={{flex: 1, flexDirection:'row', alignItems: 'center'}}>
+                    <Image style={{alignSelf: 'center'}} source={{uri: userData.driver_photo }} style={{height: 200, width: 200, flex: 1}}/>
+                  </View>
+                }
+                
+                <List>
+                  <ListItem >
+                    <Text>Name - { userData.driver_name }</Text>
+                  </ListItem>
+                  <ListItem>
+                    <Text>Contact- { userData.driver_contact }</Text>
+                  </ListItem>
+                  <ListItem>
+                    <Text>Address - { userData.driver_address }</Text>
+                  </ListItem>
+                  <ListItem>
+                    <Text>Vehicle Number - { userData.driver_carno }</Text>
+                  </ListItem>
+                  <ListItem>
+                    <Text>License Number - { userData.driver_licence}</Text>
+                  </ListItem>
+                  <ListItem>
+                    <Text>Insurance Expiry Date - { userData.driver_expiry }</Text>
+                  </ListItem>
+                </List>
+              </Body>
+            </CardItem>
+          </Card>
         </Content>
       </Container>
     );
   }
 }
 
-export default Profile;
+export default connect (mapStateToProps) (Profile);
